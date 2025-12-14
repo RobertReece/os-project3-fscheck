2cf4e2b6fd7da2a84aa6566d36edc283a55aef4e
Wesley Baskett <wesbaskett@gmail.com>
2025-12-13 20:55:14 -0600

    Added tests for valid inode, valide root directory, and valid . & .. directories. Block bitmap check in progress

--- fcheck_helper.c
+++ fcheck_helper.c
@@ -14,15 +14,91 @@
 
 #define BLOCK_SIZE (BSIZE)
 
+#define INODE_ADDR(i) ((struct dinode *)(addr + IBLOCK(i) * BLOCK_SIZE) + ((i) % IPB))
 
-int
-main(int argc, char *argv[])
-{
+char *addr;
+struct dinode *dip;
+struct superblock *sb;
+struct dirent *de;
+
+//Function for test case 1
+//this function checks what type is on the inode 
+//if it's not a valid type check if its unallocated
+//if its both invalid type and allocated, throw error
+void check_valid_inode(struct dinode *ip){
+	if (ip->type < 1 || ip->type > 3){
+		if (ip-> type == 0 && ip->size == 0 && ip->nlink == 0) { return; }
+		fprintf(stderr, "ERROR: bad inode.\n");
+		exit(1);
+	}
+}
+
+// Function to read the value for block in the bitmap
+// returns  and integer 1/0 (allocated/not allocated)
+int get_bit(int block_number) {
+	int bitmap_block = BBLOCK(block_number, sb->ninodes); 
+	unsigned char *bitmap = (unsigned char *)(addr + bitmap_block * BLOCK_SIZE);
+	// Get the byte and check the bit corresponding to the block
+	return (bitmap[block_number / 8] >> (block_number % 8)) & 1;
+}
+
+//Helper function to recursively traverse directories from the given inode
+//this function assumes that we've already validated every inode
+void print_directory_contents(int dir_inum) {
+	bool found_parent = false;
+	bool found_self = false;
+	// Access the inode for the directory
+	struct dinode *dip = INODE_ADDR(dir_inum);
+
+	// Only process if it's a directory (type == 1)
+	if (dip->type != 1) {
+		return;
+	}
+    
+	// Read the directory blocks
+	struct dirent *de = (struct dirent *)(addr + (dip->addrs[0]) * BLOCK_SIZE);
+	int num_entries = dip->size / sizeof(struct dirent);
+    
+	// Loop through the directory entries and print them
+	for (int i = 0; i < num_entries; i++, de++) {
+	
+		// If the entry is a directory, print its contents recursively
+		if (de->inum != 0) {
+			printf("inum %d, name %s\n", de->inum, de->name);
+
+			//Skip "." and ".." directory entries and note that we found them
+			if (strcmp(de->name, ".") == 0){
+				if (de->inum != dir_inum){
+					fprintf(stderr, "ERROR: directory not properly formatted.\n");
+				}
+				found_self = true;
+				continue;
+			} else if (strcmp(de->name, "..") == 0) {
+				found_parent = true;
+				//If we're curretnly in the root dir, check that .. is the root dir still
+				if (dir_inum == ROOTINO && de->inum != dir_inum){
+					fprintf(stderr, "ERROR: root directory does not exist.\n");
+					exit(1);
+				}
+				continue;
+			}
+			
+			struct dinode *entry_inode = INODE_ADDR(de->inum);
+
+			// If it's a directory, recurse
+			if (entry_inode->type == 1){
+				printf("  Recursively listing directory: %s\n", de->name);
+				print_directory_contents(de->inum);
+			}
+		}
+	}
+	if (found_self && found_parent) { return;}
+	fprintf(stderr, "ERROR: directory not properly formatted.\n");
+	exit(1);
+}
+
+int main(int argc, char *argv[]) {
   int r,i,n,fsfd;
-  char *addr;
-  struct dinode *dip;
-  struct superblock *sb;
-  struct dirent *de;
 
   if(argc < 2){
     fprintf(stderr, "Usage: sample fs.img ...\n");
@@ -36,8 +112,9 @@ main(int argc, char *argv[])
     exit(1);
   }
 
-  /* Dont hard code the size of file. Use fstat to get the size */
-  addr = mmap(NULL, 524248, PROT_READ, MAP_PRIVATE, fsfd, 0);
+  struct stat st;
+  fstat(fsfd, &st);
+  addr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fsfd, 0);
   if (addr == MAP_FAILED){
 	perror("mmap failed");
 	exit(1);
@@ -52,7 +129,7 @@ main(int argc, char *argv[])
 
   // read root inode
   printf("Root inode  size %d links %d type %d \n", dip[ROOTINO].size, dip[ROOTINO].nlink, dip[ROOTINO].type);
-
+  /*
   // get the address of root dir 
   de = (struct dirent *) (addr + (dip[ROOTINO].addrs[0])*BLOCK_SIZE);
 
@@ -60,9 +137,51 @@ main(int argc, char *argv[])
 
   n = dip[ROOTINO].size/sizeof(struct dirent);
   for (i = 0; i < n; i++,de++){
- 	printf(" inum %d, name %s ", de->inum, de->name);
-  	printf("inode  size %d links %d type %d \n", dip[de->inum].size, dip[de->inum].nlink, dip[de->inum].type);
+ 	printf(" inum %d, name %s address %p", de->inum, de->name, de);
+    	printf("inode  size %d links %d type %d \n", dip[de->inum].size, dip[de->inum].nlink, dip[de->inum].type);
   }
+  */
+  
+  
+  //Loop over every inode
+  int inum;
+  for(inum = 1; inum < sb->ninodes; inum++) {
+	struct dinode *ip = INODE_ADDR(inum);
+	check_valid_inode(ip);
+ 	if (inum == 1 && ip->size == 0){
+		fprintf(stderr, "ERROR: root directory does not exist.\n");
+		exit(1);
+	}
+	if(ip->type  == 0) {continue;}//skip over unallocated inodes
+
+	//check that all of the inodes  blocks are present in the bitmap
+	for (i = 0; i < NDIRECT; i++) {
+		int block = ip->addrs[i];
+		if (block != 0) {
+			int alloc = get_bit(block);			
+			printf("Block %d used by inode %d (direct) Allocated: %d\n", block, inum, alloc);
+			if (alloc != 1) { 
+				fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
+				exit(1);
+			}
+		}
+	}
+
+	// If the inode has an indirect block, read the indirect block
+	if (inode->addrs[NDIRECT] != 0) {
+		
+		int indirect_block = inode->addrs[NDIRECT];
+		// TODO:  LOOP THROUGH	
+	}
+
+	printf("inode %d: type %d size %d nlink %d\n", inum, ip->type, ip->size, ip->nlink);
+
+  }
+
+
+
+  print_directory_contents(ROOTINO);
+
   exit(0);
 
 }
